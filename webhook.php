3ac6818e0dafca98a048948712a3b01e95fa059b
Kaohorm45 <78629517+Kaohorm45@users.noreply.github.com>
2021-11-03 15:24:39 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -22,9 +22,13 @@ foreach ($request_json['events'] as $event)
 			if(("เส้นทางไปมหาวิทยาลัยพระจอมเกล้าธนบุรี"==$text) || ("ไปมหาลัยบางมด"==$text) || ("เส้นทางไปมหาวิทยาลับยางมด"==$text) || ("ไปมจธ."==$text) ){
 				$reply_message = "เส้นทางไปมหาวิทยาลัยพระจอมเกล้าธนบุรี" ;
 				
-				$result = https://www.google.com/maps/place/King+Mongkut%E2%80%99s+University+of+Technology+Thonburi/@13.6512734,100.4943349,17z/data=!3m1!4b1!4m5!3m4!1s0x30e2a251bb6b0cf1:0xf656e94ff13324ad!8m2!3d13.6512734!4d100.4965236?hl=en');   // Get request content
-
-                               
+				$result = https://www.google.com/maps/place/King+Mongkut%E2%80%99s+University+of+Technology+Thonburi/@13.6512734,100.4943349,17z/data=!3m1!4b1!4m5!3m4!1s0x30e2a251bb6b0cf1:0xf656e94ff13324ad!8m2!3d13.6512734!4d100.4965236?hl=en');   // Get request content	
+			
+			 $result_json = json_decode($result, true);   // Decode JSON request
+				
+				
+				
+				
 				
 			}
 		
